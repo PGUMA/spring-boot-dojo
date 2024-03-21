010e6884cd4ad6bf7f6938f7b666c15aed88c216
pguma <68600968+PGUMA@users.noreply.github.com>
2024-03-21 22:38:41 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
+
+	id("io.gitlab.arturbosch.detekt") version "1.23.5"
+}
+
+group = "lab.pguma.spring"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-batch")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.batch:spring-batch-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "21"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+detekt {
+	source.setFrom("src/main/kotlin")
+}
+
+tasks.withType<Detekt>().configureEach {
+	jvmTarget = "21"
+	reports {
+		html.required.set(true)
+	}
+}
